a022b6a3c646963298f73043e76eb03689846a7e
LeonelaRivarola <leonelarivarola6322@gmail.com>
2025-09-01 09:41:10 -0300

    iniciando proyecto

---
+++ src/main/java/com/ayds2/Proyecto/ayds2/ProyectoAyds2Application.java
@@ -0,0 +1,13 @@
+package com.ayds2.Proyecto.ayds2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAyds2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAyds2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayds2/Proyecto/ayds2/ProyectoAyds2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayds2.Proyecto.ayds2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAyds2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
